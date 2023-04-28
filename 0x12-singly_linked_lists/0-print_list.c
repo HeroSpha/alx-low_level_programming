9b318703ff63e38744c860eb6344f9ea18babc6a
Siphamandla Ngwenya <ngwenyaspa@gmail.com>
2023-04-28 12:38:01 -0700

    changed data

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,14 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked 
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -15,8 +18,10 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		size++;
+		s++;
 	}
 
-	return (size);
+	return (s);
 }
+
+
