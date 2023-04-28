247e299643b8f733ffb826998548991595cc9bd5
Siphamandla Ngwenya <ngwenyaspa@gmail.com>
2023-04-28 13:21:08 -0700

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	size_t x = 0;
 
 	while (h)
 	{
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		size++;
+		x++;
 	}
 
-	return (size);
+	return (x);
 }
 
